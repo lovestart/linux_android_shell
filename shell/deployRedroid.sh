2843d8bc94786507f3f4dc4f8ac2f8dfb4e66d0c
hzl <huangzilong@sinozo.com>
2024-03-15 14:11:06 +0800

    更新模拟器部署脚本

--- shell/deployRedroid.sh
+++ shell/deployRedroid.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 set -e
 
+# 获取设备的ip和端口
+findDeviceIpPort() {
+  local containerName="$1"
+  deviceIp=$(docker exec "$containerName" ifconfig eth0 | grep "inet " | awk -F '[: ]+' '{print $4}')
+  devicePort=$(docker port "$containerName" | head -n 1 | awk '{split($3,a,":"); print a[length(a)]}')
+  if [ -z "$deviceIp" ] || [ -z "$devicePort" ]; then
+    deviceIpPort=""
+  else
+    deviceIpPort="$deviceIp:$devicePort"
+  fi
+  echo "$deviceIpPort"
+}
+
+# 连接设备
+connectDevice() {
+  local containerName="$1"
+  connected=false
+  counter=1
+  while [ $counter -le 10 ]
+  do
+    deviceIp=$(findDeviceIpPort "$containerName")
+    if ! [ -z "$deviceIp" ]; then
+      result=$(docker exec ws-scrcpy adb connect "$deviceIp")
+      if [ -z "$(echo "$result" | grep "connected")" ]; then
+          echo "连接失败：$result"
+      else
+          echo "成功连接！"
+          connected=true
+          break
+      fi
+    fi
+    counter=$((counter + 1))
+    sleep 1
+  done
+
+  if [ "$connected" = true ]; then
+    return 0
+  else
+    echo "设备连接失败。退出脚本"
+    return 1
+  fi
+}
+
+
 if dpkg -l | grep -q "docker"; then
     echo "Docker 已安装"
 else
@@ -28,6 +72,7 @@ scrcpy_dir="/home/ubuntu/apk"
 if [ ! -d "$scrcpy_dir" ]; then
   mkdir -p "$scrcpy_dir"
 fi
+
 # 安装scrcpy用来远程控制模拟器
 if ! docker ps -a --format '{{.Names}}' | grep -q "ws-scrcpy"; then
   docker run -it -v /home/ubuntu/apk:/apk --name ws-scrcpy -d -p 8000:8000 scavin/ws-scrcpy
@@ -39,39 +84,26 @@ apt install linux-modules-extra-`uname -r`
 modprobe binder_linux devices="binder,hwbinder,vndbinder"
 # modprobe ashmem_linux
 
-if ! docker ps -a --format '{{.Names}}' | grep -q "redroid11"; then
+containerName="redroid11"
+if ! docker ps -a --format '{{.Names}}' | grep -q "$containerName"; then
   docker run -itd --privileged \
     -v ~/data11:/data \
     -p 5555:5555 \
-    --name redroid11 \
+    --name "$containerName" \
     redroid/redroid:11.0.0-latest
 else
-  docker restart redroid11
+  docker restart "$containerName"
 fi
 
+# 先连接一遍设备，确认设备启动成功
+connectDevice "$containerName"
 
+# 获取设备的root权限
+deviceIpPort=$(findDeviceIpPort "$containerName")
+echo "$(docker exec ws-scrcpy adb -s "$deviceIpPort" root)"
 
-connected=false
-counter=1
-while [ $counter -le 10 ]
-do
-    deviceIp=$(docker exec redroid11 ifconfig eth0 | grep "inet " | awk -F '[: ]+' '{print $4}')
-    result=$(docker exec ws-scrcpy adb connect "$deviceIp:5555")
-    if [ -z "$(echo "$result" | grep "connected")" ]; then
-        echo "连接失败：$result"
-    else
-        echo "成功连接！"
-        connected=true
-        break
-    fi
-    counter=$((counter + 1))
-    sleep 1
-done
-
-if [ "$connected" = false ]; then
-    echo "无法连接到设备。"
-    exit 1
-fi
+# 连接设备
+connectDevice "$containerName"
 
 serverIp=$(curl -s ifconfig.me)
 echo "浏览器打开'$serverIp:8000'查看设备列表"
